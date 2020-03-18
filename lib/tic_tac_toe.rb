30ed09253eedb648e8498f6fc764c401e734cac1
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-18 22:56:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,6 +9,9 @@ WIN_COMBINATIONS = [
  [0, 4, 8],
  [6, 4, 2]
 ]
+def initialize
+  @board = Array.new(9, " ")
+end
 
 def display_board
   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
@@ -16,10 +19,6 @@ def display_board
   puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
   puts "-----------"
   puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-end
-
-def initialize
-  @board = Array.new(9, " ")
 end
 
 
