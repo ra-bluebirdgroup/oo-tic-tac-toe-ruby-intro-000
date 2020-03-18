3bc29322ca024237dd5b2dde5937748b295cd0bc
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-18 22:55:06 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,7 +16,12 @@ def display_board
   puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
   puts "-----------"
   puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+end 
+
+def initialize
+  @board = Array.new(9, " ")
 end
+
 
 def input_to_index(user_input)
  user_input.to_i - 1
